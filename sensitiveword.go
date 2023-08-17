d38f0528191e21ff643070c9f17d5e347f1f7c79
王守伟 <wsw@dataqin.com>
2023-08-17 16:40:04 +0800

    update

--- sensitiveword.go
+++ sensitiveword.go
@@ -1,5 +1,7 @@
 package sensitiveword
 
+var _ SensitiveFilter = (*SensitiveFilterStruct)(nil)
+
 // 敏感词过滤
 type SensitiveFilter interface {
 	Filter(input string) (string, error)
@@ -8,6 +10,10 @@ type SensitiveFilter interface {
 // 实现敏感词过滤接口
 
 type SensitiveFilterStruct struct {
+}
+
+func NewSensitiveFilterStruct() SensitiveFilter {
+	return &SensitiveFilterStruct{}
 }
 
 func (s *SensitiveFilterStruct) Filter(input string) (string, error) {
